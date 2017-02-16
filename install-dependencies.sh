a34d58d9e3fac29788e259c4fdd438786c4aa5db
Morten Kristiansen <morten.kristiansen@gmail.com>
2017-02-16 09:05:15 +0000

    Adds additional echo lines for progress info

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv 7F0CEB1
 sudo echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo cp /vagrant/mongod.service /lib/systemd/system/
+#sudo cp /vagrant/mongod.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable mongod
 sudo systemctl start mongod
