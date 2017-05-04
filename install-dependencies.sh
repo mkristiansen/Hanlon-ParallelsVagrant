5faaed6ea8f2562b52b395508d31256cad4422fa
Morten Kristiansen <morten.kristiansen@gmail.com>
2017-05-04 15:57:49 +0100

    # Summary (50 characters or less) Cosmetic changes in comments # Why did you make this change
    # Relevant documentation
    
    # Is this related to issues / tickets ?

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv 7F0CEB1
 sudo echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-#sudo cp /vagrant/mongod.service /lib/systemd/system/
+# sudo cp /vagrant/mongod.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable mongod
 sudo systemctl start mongod
@@ -19,13 +19,14 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 sudo apt-get install -y docker-engine
-# Start docker-engine
+
+echo "[INFO] Starting Docker Engine"
 sudo systemctl start docker
 
 echo "[INFO] Installing: DHCP, TFT and iPXE Services"
 sudo apt-get install -y isc-dhcp-server ipxe tftp tftpd
 
-#Configure tftpd
+echo "[INFO] Configuring TFTPD"
 [ ! -d /tftpboot ] && sudo mkdir /tftpboot
 sudo chmod -R 777 /tftpboot
 sudo chown -R nobody /tftpboot
@@ -33,7 +34,8 @@ sudo chown -R nobody /tftpboot
 sudo cp /vagrant/tftp /etc/xinetd.d/tftp
 sudo service xinetd reload
 
-#Copy ipxe files to tftpboot
+# Copy ipxe files to tftpboot
+echo "[INFO] Configuring iPXE"
 cp -r /usr/lib/ipxe/* /tftpboot
 
 #Patch general.h - only needed for ESXi
@@ -43,7 +45,7 @@ cp -r /usr/lib/ipxe/* /tftpboot
 #patch -p0 < image_comboot.patch
 #rm image_comboot.patch
 
-# Install pxelinux
+echo "[INFO] Installing PXELinux"
 wget -nv https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.02.tar.gz
 tar -zxvf syslinux-6.02.tar.gz --strip-components 3 -C /tftpboot syslinux-6.02/bios/core/pxelinux.0
 tar -zxvf syslinux-6.02.tar.gz --strip-components 4 -C /tftpboot syslinux-6.02/bios/com32/menu/menu.c32
@@ -51,6 +53,7 @@ tar -zxvf syslinux-6.02.tar.gz --strip-components 4 -C /tftpboot syslinux-6.02/b
 [ ! -d /tftpboot/pxelinux.cfg ] && mkdir /tftpboot/pxelinux.cfg
 sudo cp /vagrant/default /tftpboot/pxelinux.cfg/default
 
-#Configure DHCPd
+
+echo "[INFO] Configuring DHCPD"
 sudo cp /vagrant/dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo service isc-dhcp-server restart
